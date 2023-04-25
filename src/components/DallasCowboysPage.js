e2e08881546935a9a69949795131ad801567ee0d
Austin <austinmeyer1106@gmail.com>
2023-04-25 13:03:35 -0500

    downloaded react package for google map integration, built out the framework for the map, and started on styling it

--- src/components/DallasCowboysPage.js
+++ src/components/DallasCowboysPage.js
@@ -2,7 +2,10 @@ import "./dallascowboyspage.css";
 import { Button } from "react-bootstrap";
 import { getCowboysTeamData } from "../api";
 import { useEffect, useState } from "react";
+import { Timeline, Tweet } from 'react-twitter-widgets'
+import GoogleMapReact from 'google-map-react';
 import cowboysLogo from "./cowboys-logo.png";
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
 
 function DallasCowboysPage({navigate}) {
 
@@ -22,10 +25,36 @@ function DallasCowboysPage({navigate}) {
   //   return cowboysTeamData;
   // }
 
+  const CowboysPageMarker = ({ text }) => (
+    <div style={{
+      color: 'white', 
+      background: 'red',
+      padding: '15px 10px',
+      display: 'inline-flex',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '100%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      {text} <span class="cowboys-page-pin-icon material-icons">push_pin</span>
+    </div>
+  );
+
+
+  const defaultProps = {
+      center: {
+        lat: 32.74907667706016,
+        lng: -97.09263245109746
+      },
+      zoom: 15
+  };
+
+
   // useEffect(() => {
   //   fetchCowboysTeamData();
   // }, [])
-
+    
   return (
     <div className="dallas-cowboys-page-container">
         <Button 
@@ -41,7 +70,22 @@ function DallasCowboysPage({navigate}) {
             <p className="cowboys-page-p1">Record: 12-5 | 2nd NFC East</p>
           </div>
         </div>
-        <h2>Hi</h2>
+        <div className="cowboys-twitter-widget">
+          {/* <Timeline dataSource={{ sourceType: "profile", screenName: "dallascowboys" }}/> */}
+        </div>
+        <div id="cowboys-page-map">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyC7ZamBBU79p5Kh6qE8SQ6N1UaYANUNac4"}}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+          <CowboysPageMarker
+            lat={32.74738363371771}
+            lng={-97.09452609047312}
+            text={"AT&T Stadium"}
+          />
+        </GoogleMapReact>
+        </div>
     </div>
   );
 }
